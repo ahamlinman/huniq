c40c2569a8e63b69fe8727d1734bf9eaec90ccb8
Alex Hamlin <alex@alexhamlin.co>
2017-10-29 21:44:55 -0700

    Implement initial version of huniq
    This is based on what I've learned so far from The Rust Programming
    Language. It implements the functionality I want, including correct
    error handling for use with fzf.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+use std::io::Write;
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let mut read_set = HashSet::new();
+
+    loop {
+        let mut line = String::new();
+
+        match io::stdin().read_line(&mut line) {
+            Err(e) => panic!(e),
+            Ok(n) => {
+                if n == 0 {
+                    return; // EOF
+                }
+            }
+        };
+
+        if !read_set.contains(&line) {
+            match io::stdout().write(line.as_bytes()) {
+                Err(ref e) if e.kind() == io::ErrorKind::BrokenPipe => return,
+                Err(e) => panic!(e),
+                Ok(_) => {
+                    read_set.insert(line);
+                }
+            };
+        }
+    }
 }
